5b8cc2a96fd0208f6e80364f570578c80ffd6b2c
keita.sasaki <jcpmb136595@yahoo.co.jp>
2020-04-26 08:15:13 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
     else
       @tasks=current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクが投稿されませんでした'
-      render 'toppages/index'
+      redirect_to root_url
     end
   end
 
