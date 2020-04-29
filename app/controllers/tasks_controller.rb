100e184f5146a0670dfd9d352dc1906e8c20680a
keita.sasaki <jcpmb136595@yahoo.co.jp>
2020-04-29 09:58:34 +0000

    modify

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show]
+  
+  
   def index
-    @task = current_user.tasks.build
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
@@ -48,11 +50,18 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-end
-
-
+  
   private
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content,:status)
   end
+  
+  def correct_user
+    @task=current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
+end
